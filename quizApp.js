2eaa7d6f6fe437dab33bb9f88593efdeab512e15
Dwerg01 <nardum@gmail.com>
2022-03-17 15:56:02 +0200

    Committing from VSCode is way different

--- quizApp.js
+++ quizApp.js
@@ -2,34 +2,43 @@
 var turn = 1;
 var score = 0;
 
-function clearQuestions () {
-    var clearActive = document.getElementsByClassName(`container`)
-    for (var item of clearActive) {
-        item.classList.remove('active')
-    }}
 
-function setQuestion (number) {
+const clearQuestions = function () {
+    var clearActive = document.getElementsByClassName(`container`);
+    for (var item of clearActive) {
+        if (item.classList[2]) {
+            var goNext = item.id.slice(-1,) + 1;
+            console.log(goNext, ": ", item.classList[2]);
+            item.classList.remove("active");
+        }}
+
+        console.log({goNext})
+        var nextQ = document.getElementById(`Question2`)
+        nextQ.classList.add('active')
+        console.log({nextQ})
+    }
+
+const setQuestion = function (number) {
     let activate = document.getElementById(`Question${number}`)
     activate.classList.add('active')
     let submitButton = document.querySelector(`#sub${number}`)
-    submitButton.addEventListener("click", NexKeshun(number))
+    submitButton.addEventListener("click", next => clearQuestions())
 }
    
-    
-function NexKeshun (number) {
+
+function scoreCount (number) {
     var target = document.querySelectorAll(`.Q-group-${turn}`);
     let submitAnswer;
     target.forEach(answer => {
         if (answer.checked) {
             submitAnswer = answer.id    
-            console.log(submitAnswer)    
+            console.log(submitAnswer) 
         }})
-
-    clearQuestions();
-    number++
 }
 
 
 (function () {
-    setQuestion(turn)
+setQuestion(turn)
 })();
+
+setQuestion(turn)
